2ac71061e7243468b978053b692622d6d82672eb
gettys12 <katemary.murphy@gmail.com>
2014-01-28 15:00:34 -0500

    test2

--- jquery-qtip2-rails/lib/jquery/qtip2/rails.rb
+++
@@ -1,9 +0,0 @@
-require "jquery/qtip2/rails/version"
-
-module Jquery
-  module Qtip2
-    module Rails
-      # Your code goes here...
-    end
-  end
-end
--- jquery-qtip2-rails/lib/jquery/qtip2/rails/version.rb
+++
@@ -1,7 +0,0 @@
-module Jquery
-  module Qtip2
-    module Rails
-      VERSION = "0.0.1"
-    end
-  end
-end
